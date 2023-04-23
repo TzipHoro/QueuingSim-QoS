82e9aa949cadfe00d86048e06a4cf974c4b01146
Rebecca (Tziporah) Horowitz <60411083+TzipHoro@users.noreply.github.com>
2023-04-23 13:37:11 -0400

    Added docstrings and reorganized simulation

--- SimulateQueue.py
+++ SimulateQueue.py
@@ -4,10 +4,19 @@ import numpy as np
 import pandas as pd
 
 
-def simulate_queue(arrival_rates: np.ndarray, service_rate: float, n_sims: int = 5000) -> pd.DataFrame:
+def simulate_queue(arrival_rates: np.ndarray, service_rate: float, until: int = 5000) -> pd.DataFrame:
+    """
+    Simulates a single server non-preemptive priority queue.
+
+    :param arrival_rates: array of arrival rates for each priority class
+    :param service_rate: service rate of the server (must be greater than the sum of arrival rates to maintain steady state)
+    :param until: the simulation will stop after n time units
+    :return: DataFrame of simulation results
+    """
     total_jobs = {}
 
     class PriorityQueue:
+        """A single server non-preemptive priority queue object."""
         def __init__(self, env: simpy.Environment):
             self.env = env
             self.queue = []
@@ -15,9 +24,20 @@ def simulate_queue(arrival_rates: np.ndarray, service_rate: float, n_sims: int =
             self.busy = False
 
         def add(self, job, priority):
+            """
+            Adds jobs to the queue while pushing higher priority jobs to the front.
+
+            :param job: item to be served
+            :param priority: priority number
+            """
             heapq.heappush(self.queue, (priority, job))
 
         def start(self, server):
+            """
+            If the queue is not empty the highest priority job is sent to service.
+
+            :param server: server object
+            """
             while True:
                 if not self.queue:
                     self.busy = False
@@ -28,11 +48,17 @@ def simulate_queue(arrival_rates: np.ndarray, service_rate: float, n_sims: int =
                     self.env.process(server.serve(job))
 
     class Server:
+        """A single server object that processes jobs given a mean service rate."""
         def __init__(self, env: simpy.Environment, svc_rate):
             self.env = env
             self.mean_service_time = svc_rate
 
         def serve(self, job):
+            """
+            Serves a single job.
+
+            :param job: item to be served
+            """
             service_time = np.random.exponential(scale=self.mean_service_time)
             yield self.env.timeout(service_time)
             job.exit_time = self.env.now
@@ -40,6 +66,7 @@ def simulate_queue(arrival_rates: np.ndarray, service_rate: float, n_sims: int =
             print(f"Job {job.__str__()} finished at time {job.exit_time}, priority {job.priority}")
 
     class Job:
+        """A job object to be serviced by the system."""
         def __init__(self, env: simpy.Environment, job_id: int, priority: int):
             self.env = env
             self.id = job_id
@@ -54,11 +81,15 @@ def simulate_queue(arrival_rates: np.ndarray, service_rate: float, n_sims: int =
             return self.priority < other.priority
 
         def start(self):
+            """
+            The job enters the queue.
+            """
             self.enter_time = self.env.now
             total_jobs[self.id] = {'priority': self.priority, 'enter_time': self.enter_time}
             print(f"Job {self.__str__()} entered the system at time {self.enter_time}, priority {self.priority}")
 
     class SimQueue:
+        """Simulates jobs entering the system and being served."""
         def __init__(self, arr_rates, svc_rate):
             self.env = simpy.Environment()
             self.arrival_rates = arr_rates
@@ -66,14 +97,25 @@ def simulate_queue(arrival_rates: np.ndarray, service_rate: float, n_sims: int =
             self.queue = PriorityQueue(self.env)
             self.job_id = 0
 
-            for i in range(4):
+            # send jobs for each class to the queue based on their arrival rates
+            for i in range(arr_rates.shape[0]):
                 self.env.process(self.generate_jobs(i))
                 self.env.process(self.process_jobs())
 
         def start(self, sim_time):
+            """
+            Starts the simulation.
+
+            :param sim_time: maximum unit of time to run the simulation until
+            """
             self.env.run(until=sim_time)
 
         def generate_jobs(self, class_id):
+            """
+            Adds jobs to the queue based on the class and its arrival rate.
+
+            :param class_id: number representing the priority
+            """
             while True:
                 interarrival_time = np.random.poisson(self.arrival_rates[class_id])
                 yield self.env.timeout(interarrival_time)
@@ -83,12 +125,15 @@ def simulate_queue(arrival_rates: np.ndarray, service_rate: float, n_sims: int =
                 job.start()
 
         def process_jobs(self):
+            """
+            When the queue is non-empty and the server is not busy, the next job gets processed.
+            """
             while True:
                 if not self.queue.busy and self.queue.queue:
                     self.env.process(self.queue.start(self.server))
                 yield self.env.timeout(1)
 
-    env = SimQueue(arr_rates=arrival_rates, svc_rate=service_rate)
-    env.start(n_sims)
+    sim_env = SimQueue(arr_rates=arrival_rates, svc_rate=service_rate)
+    sim_env.start(until)
 
     return pd.DataFrame(total_jobs).T
--- plots.py
+++ plots.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pandas as pd
 import seaborn as sns
-from matplotlib import rcParams
 
 from SimulateQueue import simulate_queue
 
@@ -19,7 +17,7 @@ if __name__ == '__main__':
     df['job'] = df.index
     df['priority'] = df['priority'].replace({0: 'low', 1: 'normal', 2: 'medium', 3: 'high'})
     df['time_in_system'] = df['exit_time'] - df['enter_time']
-    df.to_csv('queue.csv', index=False)
+    df.to_csv('simulations/queue.csv', index=False)
 
     # system time plot
     sns.set(rc={'figure.figsize': (10, 5)})
@@ -30,26 +28,26 @@ if __name__ == '__main__':
     plot.get_figure().clf()
 
     # latency
-    # latency = df.groupby('priority').agg(['min', 'max', 'mean'])['time_in_system']
-    # latency.to_csv('latency.csv')
-    # normalized_latency = latency.apply(lambda x: (x - x.mean()) / x.std())
-    # normalized_latency.reset_index(inplace=True)
-    # normalized_latency = normalized_latency.melt(id_vars='priority')
-    #
-    # sns.set()
-    # plot = sns.barplot(normalized_latency, x='priority', y='value', hue='variable', palette='flare',
-    #                    order=['low', 'normal', 'medium', 'high'])
-    # plot.set(xlabel='Priority', ylabel='Latency', title='Network Latency')
-    # plot.get_figure().savefig('latency.png', dpi=400)
-    # plot.get_figure().clf()
-    #
-    # # throughput
-    # throughput = np.ceil(df['exit_time']).value_counts()
-    # throughput = throughput.describe().to_frame('throughput').loc[['min', 'max', 'mean']]
-    # throughput['temp'] = None
-    # throughput = throughput.reset_index().melt(id_vars='index').dropna()
-    #
-    # plot = sns.barplot(throughput, x='variable', y='value', hue='index', palette='flare')
-    # plot.set(xlabel='', title='Network Throughput')
-    # plot.get_figure().savefig('throughput.png', dpi=400)
-    # plot.get_figure().clf()
+    latency = df.groupby('priority').agg(['min', 'max', 'mean'])['time_in_system']
+    latency.to_csv('simulations/latency.csv')
+    normalized_latency = latency.apply(lambda x: (x - x.mean()) / x.std())
+    normalized_latency.reset_index(inplace=True)
+    normalized_latency = normalized_latency.melt(id_vars='priority')
+
+    sns.set()
+    plot = sns.barplot(normalized_latency, x='priority', y='value', hue='variable', palette='flare',
+                       order=['low', 'normal', 'medium', 'high'])
+    plot.set(xlabel='Priority', ylabel='Latency', title='Network Latency')
+    plot.get_figure().savefig('plots/latency.png', dpi=400)
+    plot.get_figure().clf()
+
+    # throughput
+    throughput = np.ceil(df['exit_time']).value_counts()
+    throughput = throughput.describe().to_frame('throughput').loc[['min', 'max', 'mean']]
+    throughput['temp'] = None
+    throughput = throughput.reset_index().melt(id_vars='index').dropna()
+
+    plot = sns.barplot(throughput, x='variable', y='value', hue='index', palette='flare')
+    plot.set(xlabel='', title='Network Throughput')
+    plot.get_figure().savefig('plots/throughput.png', dpi=400)
+    plot.get_figure().clf()
